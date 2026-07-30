f6b24288b7377a6c57eeacb2a2762784a6c2935b
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-07-30 05:43:36 +0000

    [v26.2.0] chore: Update CodeSnifferInfo for the release

--- src/CodeSniffer/Constant/CodeSnifferInfo.php
+++ src/CodeSniffer/Constant/CodeSnifferInfo.php
@@ -20,12 +20,12 @@ final class CodeSnifferInfo
      *
      * @var non-empty-string
      */
-    public const string VERSION = '26.1.3';
+    public const string VERSION = '26.2.0';
 
     /**
      * The CodeSniffer package version build datetime.
      *
      * @var non-empty-string
      */
-    public const string VERSION_BUILD_DATE_TIME = 'July 23 2026 08:42:01 MST';
+    public const string VERSION_BUILD_DATE_TIME = 'July 29 2026 22:42:51 MST';
 }
